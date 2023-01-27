cc7b209066a7a24b48e1b29e07567378d6b66eb3
Adam Gonzales <adamgonzales1@gmail.com>
2023-01-27 12:14:59 -0700

    feature: add dispense_drink() and dispense_change() - basic functionality complete - add flowchart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from data import MENU, resources
-# print(MENU)
 
 revenue = 0
 
@@ -26,10 +25,31 @@ def check_drink_resources(selected_drink_ingredients):
 
 def tender_payment():
     print('Please insert coins.')
+    total_tendered = 0
     quarters_amount = int(input("How many quarters?: "))
     dimes_amount = int(input("How many dimes?: "))
     nickels_amount = int(input("How many nickels?: "))
     pennies_amount = int(input("How many pennies?: "))
+
+    total_tendered += quarters_amount * .25
+    total_tendered += dimes_amount * .10
+    total_tendered += nickels_amount * .05
+    total_tendered += pennies_amount * .01
+    print(f"You have input {total_tendered}")
+    return total_tendered
+
+
+def dispense_drink(selected_drink_ingredients):
+    for ingredient in selected_drink_ingredients:
+        resources[ingredient] -= selected_drink_ingredients[ingredient]
+        print(f'subtract {selected_drink_ingredients[ingredient]} from resource {resources[ingredient]} here')
+
+
+def dispense_change(money_deposited, selected_drink_cost):
+    print(f"Amount deposited: {money_deposited} Drink cost: {selected_drink_cost}")
+    global revenue
+    revenue += selected_drink_cost
+    return round(money_deposited - selected_drink_cost, 2)
 
 
 is_on = True
@@ -42,18 +62,17 @@ while is_on:
         show_resources()
     elif user_selection in MENU:
         selected_drink_ingredients = MENU[user_selection]['ingredients']
-        print("You chose a drink.")
         if check_drink_resources(selected_drink_ingredients):
-            tender_payment()
+            selected_drink_cost = MENU[user_selection]['cost']
+            money_deposited = tender_payment()
+            if money_deposited >= selected_drink_cost:
+                dispense_drink(selected_drink_ingredients)
+                users_change = dispense_change(money_deposited, selected_drink_cost)
+                print(f"Here is {users_change} in change. Here is your {user_selection}. Enjoy!")
+            else:
+                print('Sorry that is not enough money. Money refunded.')
     elif user_selection == 'off':
+        print("Powering off machine.")
         is_on = False
     else:
         print("Please choose a valid selection.")
-
-# coin operated - penny, dime, nickel, quarter
-# program requirements:
-# 1. print report (how many resources are left remaining),
-# 2. check resources are sufficient
-# 3. process coins
-# 4. check transaction successful.
-# 5. Make coffee (deduct resources)
